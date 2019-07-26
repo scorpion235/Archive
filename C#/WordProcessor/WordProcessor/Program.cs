84d2de8d091c041808174da8ce7bb3f916752d22
scorpion235 <scorpion235@mail.ru>
2019-07-26 16:14:26 +0700

    EXECUTE PROCEDURE PKB_WORDS.INSERT_DATA

--- C#/WordProcessor/WordProcessor/Program.cs
+++ C#/WordProcessor/WordProcessor/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using FirebirdSql.Data.FirebirdClient;
 
 namespace WordProcessor
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
+            string connectionString = ConfigurationManager.ConnectionStrings["FBConnection"].ConnectionString;
+
+            FbConnection myConnection = new FbConnection(connectionString);
+            
+            try
+            {
+                myConnection.Open();
+
+                FbTransaction myTransaction = myConnection.BeginTransaction();
+
+                FbCommand myCommand = new FbCommand();
+                myCommand.CommandText = "EXECUTE PROCEDURE PKB_WORDS.INSERT_DATA(@WORD)";
+                myCommand.Connection = myConnection;
+                myCommand.Transaction = myTransaction;
+                myCommand.Parameters.Add("@WORD", FbDbType.VarChar);
+                myCommand.Parameters[0].Value = "тест";
+                myCommand.ExecuteNonQuery();
+
+                myTransaction.Commit();
+                myCommand.Dispose();
+                myConnection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
